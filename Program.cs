7269b0b6c4ead8d81d3842041e5af32dc6357333
JohanH20 <54435441+JohanH20@users.noreply.github.com>
2019-10-01 13:50:23 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Arrayer_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] tal = new int[5];
+            
+            for(int i = 0; i < tal.GetLength(0); i++)
+            {
+                Console.Write("Skriv ett tal: ");
+                String x = Console.ReadLine();
+                int.TryParse(x, out int Tal);
+                tal[i] = Tal;
+            }
+            Console.WriteLine("Din array baklänges: " + tal[4] + tal[3]+ tal[2]+ tal[1]+ tal[0]);
+        }
+    }
+}
